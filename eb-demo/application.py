7ced5a465eb35dac0cc042ed1179a1031cd232ef
miyamoto <midaisuk@gmail.com>
2018-06-21 07:58:01 +0000

    100 / 1000

--- eb-demo/application.py
+++ eb-demo/application.py
@@ -3,10 +3,11 @@ import json
 import boto3
 import sys
 from datetime import datetime
+from time import sleep
 
 
 class Kinesis():
-    def __init__(self, region='ap-northeast-1', stream_name='demo-aws-ad-stream'):
+    def __init__(self, stream_name, region='ap-northeast-1'):
         self.kinesis_client = boto3.client('kinesis', region_name=region)
         self.stream_name = stream_name
     
@@ -19,32 +20,54 @@ class Kinesis():
         return response
 
 
+class KinesisFirehose():
+    def __init__(self, stream_name, region='ap-northeast-1'):
+        self.kinesis_client = boto3.client('firehose', region_name=region)
+        self.stream_name = stream_name
+    
+    def put(self, data):
+        response = self.kinesis_client.put_record(
+            DeliveryStreamName=self.stream_name,
+            Record={
+                'Data': data
+            }
+        )
+        return response
+
+
 class UserTable():
     def __init__(self):
         self.ad_path = ''
         self.default_ad_uri = 'ad0.jpeg'
         self.users = {
             1: {
+                'user_id': '1',
                 'country code': 'US',
                 'state/region': 'WA',
+                'ad_id': '1',
                 'ad_uri': 'ad1.jpeg'
             },
             2: {
+                'user_id': '2',
                 'country code': 'US',
                 'state/region': 'NY',
+                'ad_id': '2',
                 'ad_uri': 'ad2.png'
             },
             3: {
+                'user_id': '3',
                 'country code': 'US',
                 'state/region': 'MA',
+                'ad_id': '1',
                 'ad_uri': 'ad1.jpeg'
             },
             4: {
+                'user_id': '4',
                 'country code': 'US',
                 'state/region': 'TX',
+                'ad_id': '1',
                 'ad_uri': 'ad1.jpeg'
             }
-
         }
         
     def get_ad_uri(self, user_id):
@@ -52,7 +75,7 @@ class UserTable():
             return self.ad_path + self.users[user_id]['ad_uri']
         else:
             return self.ad_path + self.default_ad_uri
-            
+
     def get_userinfo_json(self, user_id):
         if user_id in self.users:
             return_dict = dict(self.users[user_id])
@@ -63,7 +86,8 @@ class UserTable():
         return json.dumps(return_dict)
 
 
-kinesis = Kinesis()
+# kinesis = Kinesis()
+kinesis = KinesisFirehose(stream_name='demo-aws-ad-firehose')
 user_table = UserTable()
 DEBUG_MODE = False
 application = Flask(__name__)
@@ -76,13 +100,14 @@ def get_root():
 
 @application.route('/ad')
 def get_ad():
+    sleep(0.04)
     try:
         user_id = int(request.args.get('user_id', '0'))
         kinesis.put(user_table.get_userinfo_json(user_id)+'\n')
         return '%s\n' % (user_table.get_ad_uri(user_id))
     except Exception as e:
         return 'Error: %s' % (e.args)
-
+        
 
 @application.route('/debug/userinfo')
 def get_userinfo():
@@ -96,6 +121,16 @@ def put_kinesis():
     send_json = user_table.get_userinfo_json(user_id)
     response = kinesis.put(send_json)
     return '%s\n%s\n' % (send_json, response)
+
+
+@application.route('/debug/adnowait')
+def get_ad_nowait():
+    try:
+        user_id = int(request.args.get('user_id', '0'))
+        kinesis.put(user_table.get_userinfo_json(user_id)+'\n')
+        return '%s\n' % (user_table.get_ad_uri(user_id))
+    except Exception as e:
+        return 'Error: %s' % (e.args)
 
 
 if __name__ == '__main__':
--- locust/locustfile.py
+++ locust/locustfile.py
@@ -1,11 +1,15 @@
 # -*- coding: utf-8 -*-
+"""
+$ ./start_slave.py -H http://hogehoge.com -c 100 -r 50
+"""
 
 from locust import HttpLocust, TaskSet, task
 from locust.events import request_success
+from datetime import datetime
 import boto3
 import json
 import time
-import datetime
+from random import random
 
 
 class KinesisFirehose():
@@ -22,6 +26,16 @@ class KinesisFirehose():
         )
         return response
 
+
+def send_kinesis_success_fire(time):
+    request_success.fire(
+        request_type='PUT',
+        name='Kinesis',
+        response_time=time,
+        response_length=0
+    )
+
+
 class SimpleSet(TaskSet):
     @task
     def hello(self):
@@ -34,54 +48,56 @@ class NowaitSet(TaskSet):
         self.client.get('/debug/adnowait?user_id=1')
 
 
+kinesis = KinesisFirehose(stream_name='demo-aws-adclick-firehose')
 class UserVarietySet(TaskSet):
+    def on_start(self):
+        self.send_click = True
+        
+        if self.send_click:
+            self.kinesis = kinesis
+
+    def _get_ad(self, user_id, ad_id=0, rate=0.1):
+        self.client.get('/ad?user_id=%d' % user_id)
+
+        if self.send_click:
+            if random() < rate:
+                start_at = time.time()
+                record = {'timestamp': datetime.utcnow().isoformat(), 'user_id': str(user_id), 'ad_id': str(ad_id)}
+                json_record = json.dumps(record)
+                response = self.kinesis.put(json_record)
+                send_kinesis_success_fire(int((time.time() - start_at) * 1000))
+
     @task(1)
     def user1(self):
-        self.client.get('/ad?user_id=1')
+        self._get_ad(1, 1, 0.1)
 
     @task(8)
     def user2(self):
-        self.client.get('/ad?user_id=2')
+        self._get_ad(2, 2, 0.2)
 
     @task(3)
     def user3(self):
-        self.client.get('/ad?user_id=3')
+        self._get_ad(3, 1, 0.05)
 
 
 class ClickWithKinesisFirehose(TaskSet):
-
     def on_start(self):
-        self.kinesis = KinesisFirehose(stream_name='demo-aws-adclick-firehose')
+        self.kinesis = kinesis
 
     @task
     def put(self):
         start_at = time.time()
-        record = {
-            'timestamp': datetime.utcnow().isoformat(),
-            'user_id': '1',
-            'ad_id': '1'
-        }
+        record = {'timestamp': datetime.utcnow().isoformat(), 'user_id': '1', 'ad_id': '1'}
         json_record = json.dumps(record)
         response = self.kinesis.put(json_record)
-        request_success.fire(
-            request_type='PUT',
-            name=self.kinesis.stream_name,
-            response_time=int((time.time() - start_at) * 1000),
-            response_length=len(response)
-        )
-
-
-class UserVarietyClickSet(TaskSet):
-
-    def on_start(self):
-        self.kinesis = boto3.client('firehose', region_name='ap-northeast-1')
+        send_kinesis_success_fire(int((time.time() - start_at) * 1000))
 
 
 class WebsiteUser(HttpLocust):
-    # task_set = UserVarietySet
+    task_set = UserVarietySet
     # task_set = SimpleSet
     # task_set = NowaitSet
-    task_set = ClickWithKinesisFirehose
+    # task_set = ClickWithKinesisFirehose
     min_wait = 0
     max_wait = 10
 
